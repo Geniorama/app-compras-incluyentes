1aff235830577f6f304a081a96f03db82ef17276
angelpublicista <angelpublicista.1@gmail.com>
2025-06-24 15:37:23 -0500

    fix: menu responsive

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -1,89 +1,204 @@
 "use client";
 
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Avatar } from "flowbite-react";
 import { logout } from "@/lib/auth";
 import { useRouter } from "next/navigation";
 import IconLogo from "@/assets/img/logo-icon.webp";
-import { TextInput } from "flowbite-react";
 import { useAuth } from "@/context/AuthContext";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { HiOutlineMenu } from "react-icons/hi";
 
 export default function DashboardNavbar() {
   const router = useRouter();
   const { user } = useAuth();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSecondaryMenuOpen, setIsSecondaryMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const displayName = [user?.firstName, user?.lastName].filter(Boolean).join(' ') || user?.displayName || user?.email?.split('@')[0] || "Usuario";
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth < 640);
+    if (typeof window !== "undefined") {
+      checkMobile(); // Comprobación inicial
+      window.addEventListener("resize", checkMobile);
+      return () => window.removeEventListener("resize", checkMobile);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isMobile) {
+      console.log("isMobile", isMobile);
+      setIsSecondaryMenuOpen(true);
+    } else {
+      console.log("isMobile", isMobile);
+      setIsSecondaryMenuOpen(false);
+    }
+  }, [isMobile]);
+
+  const displayName =
+    [user?.firstName, user?.lastName].filter(Boolean).join(" ") ||
+    user?.displayName ||
+    user?.email?.split("@")[0] ||
+    "Usuario";
   const email = user?.email || "";
 
   // Obtener la URL de la imagen de perfil desde Sanity si existe
-  const avatarUrl = user?.photo && user?.photo.asset?._ref
-    ? `https://cdn.sanity.io/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/${user.photo.asset._ref
-        .replace('image-', '')
-        .replace('-jpg', '.jpg')
-        .replace('-png', '.png')
-        .replace('-webp', '.webp')}`
-    : "https://flowbite.com/docs/images/people/profile-picture-5.jpg";
+  const avatarUrl =
+    user?.photo && user?.photo.asset?._ref
+      ? `https://cdn.sanity.io/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/${user.photo.asset._ref
+          .replace("image-", "")
+          .replace("-jpg", ".jpg")
+          .replace("-png", ".png")
+          .replace("-webp", ".webp")}`
+      : "https://flowbite.com/docs/images/people/profile-picture-5.jpg";
 
   const handleSignOut = async () => {
     try {
       await logout();
-      localStorage.removeItem('authToken');
-      sessionStorage.removeItem('authToken');
+      localStorage.removeItem("authToken");
+      sessionStorage.removeItem("authToken");
       router.push("/login");
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
     }
   };
 
+  const MenuSecondary = () => {
+    return (
+      <div className={`flex justify-between items-center w-full sm:w-auto`}>
+        <nav className="flex justify-center items-center w-full sm:w-auto">
+          <ul className={`flex items-center gap-4 flex-col sm:flex-row ${isMobile && "w-full mt-5"}`}>
+            <li className="hidden sm:block">
+              <Link href="/dashboard">
+                <img alt="Logo" src={IconLogo.src} className="h-6 sm:h-8" />
+              </Link>
+            </li>
+            <li className={`${isMobile && "border-b border-gray-200 pb-4 w-full"}`}>
+              <Link href="/dashboard">Empresas</Link>
+            </li>
+            <li className={`${isMobile && "pb-2 w-full"}`}>
+              <Link href="/dashboard">Productos y Servicios</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+
   return (
-    <Navbar className="fixed w-full z-30 bg-white border-b border-gray-200">
-      <div className="flex gap-4 md:order-2">
-        <TextInput
-          placeholder="Buscar..."
-          className="w-full min-w-72"
-        />
-        <div>
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar
-                alt="User settings"
-                img={avatarUrl}
-                rounded
-                className="w-10 h-10"
-              />
+    <div className="flex max-w-[100vw] justify-between items-center px-4 py-3 bg-white border-b border-gray-200 fixed w-full z-30 sm:flex-row flex-col-reverse">
+      {isSecondaryMenuOpen && <MenuSecondary />}
+      
+      <div className="flex sm:items-center gap-4 w-full sm:w-auto">
+        {/* Logo mobile */}
+        <div className="flex justify-start items-center w-full sm:hidden">
+          <Link href="/dashboard">
+            <img alt="Logo" src={IconLogo.src} className="h-12" />
+          </Link>
+        </div>
+
+        {/* Menu mobile */}
+        <div className="flex justify-end items-center w-full sm:hidden">
+          <button className="outline-none" onClick={() => {
+            setIsSecondaryMenuOpen(!isSecondaryMenuOpen)
+            setIsMenuOpen(false)
+          }}>
+            <HiOutlineMenu className="h-6 w-6" />
+          </button>
+        </div>
+
+        <div className="relative flex justify-center items-center">
+          <button className="outline-none" onClick={() => {
+            setIsMenuOpen(!isMenuOpen)
+            if(isMobile) {
+              setIsSecondaryMenuOpen(false)
             }
+          }}>
+            <Avatar
+              alt="User settings"
+              img={avatarUrl}
+              rounded
+              className="w-10 h-10"
+            />
+          </button>
+
+          <div
+            className={`absolute top-10 right-0 bg-white shadow-md rounded-md p-2 w-60 z-10 ${isMenuOpen ? "block" : "hidden"}`}
           >
-            <Dropdown.Header>
-              <span className="block text-sm">{displayName}</span>
-              <span className="block truncate text-sm font-medium">{email}</span>
-            </Dropdown.Header>
-            <Dropdown.Item onClick={() => router.push("/dashboard/perfil")}>
-              Perfil
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={() => router.push("/dashboard/productos")}>
-              Mis productos y servicios
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={() => router.push("/dashboard/mensajes")} className="flex items-center justify-between">
-              <span>Mensajes</span>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={handleSignOut}>Cerrar sesión</Dropdown.Item>
-          </Dropdown>
-          <Navbar.Toggle />
+            <div className="flex flex-col gap-1 p-2">
+              <span className="text-sm block font-medium text-gray-900">{displayName}</span>
+              <span className="text-sm block text-gray-500">{email}</span>
+            </div>
+            <hr className="my-2" />
+            <ul className="flex flex-col gap-2">
+              <li className="hover:bg-gray-100 p-2 rounded-md">
+                <Link href="/dashboard/perfil">Perfil</Link>
+              </li>
+              <li className="hover:bg-gray-100 p-2 rounded-md">
+                <Link href="/dashboard/productos">
+                  Mis productos y servicios
+                </Link>
+              </li>
+              <li className="hover:bg-gray-100 p-2 rounded-md">
+                <Link href="/dashboard/mensajes">Mensajes</Link>
+              </li>
+              <li className="hover:bg-gray-100 p-2 rounded-md">
+                <button onClick={handleSignOut}>Cerrar sesión</button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="flex md:order-1 items-center gap-3">
-        <Navbar.Brand href="/dashboard">
-            <img alt="Logo" src={IconLogo.src} className="h-6 sm:h-9" />
-        </Navbar.Brand>
-        <Navbar.Collapse>
-            <Navbar.Link href="/empresas">Empresas</Navbar.Link>
-            <Navbar.Link href="/catalogo">Productos y Servicios</Navbar.Link>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+    </div>
+    // <Navbar className="fixed w-full z-30 bg-white border-b border-gray-200 flex-col">
+    //   <div className="flex gap-4 md:order-2">
+    //     <TextInput
+    //       placeholder="Buscar..."
+    //       className="w-full min-w-72"
+    //     />
+    //     <div>
+    //       <Dropdown
+    //         arrowIcon={false}
+    //         inline
+    //         label={
+    //           <Avatar
+    //             alt="User settings"
+    //             img={avatarUrl}
+    //             rounded
+    //             className="w-10 h-10"
+    //           />
+    //         }
+    //       >
+    //         <Dropdown.Header>
+    //           <span className="block text-sm">{displayName}</span>
+    //           <span className="block truncate text-sm font-medium">{email}</span>
+    //         </Dropdown.Header>
+    //         <Dropdown.Item onClick={() => router.push("/dashboard/perfil")}>
+    //           Perfil
+    //         </Dropdown.Item>
+    //         <Dropdown.Divider />
+    //         <Dropdown.Item onClick={() => router.push("/dashboard/productos")}>
+    //           Mis productos y servicios
+    //         </Dropdown.Item>
+    //         <Dropdown.Divider />
+    //         <Dropdown.Item onClick={() => router.push("/dashboard/mensajes")} className="flex items-center justify-between">
+    //           <span>Mensajes</span>
+    //         </Dropdown.Item>
+    //         <Dropdown.Divider />
+    //         <Dropdown.Item onClick={handleSignOut}>Cerrar sesión</Dropdown.Item>
+    //       </Dropdown>
+    //       <Navbar.Toggle />
+    //     </div>
+    //   </div>
+    //   <div className="flex md:order-1 items-center gap-3">
+    //     <Navbar.Brand className="hidden sm:block" href="/dashboard">
+    //         <img alt="Logo" src={IconLogo.src} className="h-6 sm:h-9" />
+    //     </Navbar.Brand>
+    //     <Navbar.Collapse>
+    //         <Navbar.Link href="/empresas">Empresas</Navbar.Link>
+    //         <Navbar.Link href="/catalogo">Productos y Servicios</Navbar.Link>
+    //     </Navbar.Collapse>
+    //   </div>
+    // </Navbar>
   );
-} 
+}
